17e84345fee1b4b515227baed334d6289ec066d8
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-02-01 17:56:45 +0000

    feat: Add settings modal to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Code to create modal is adapted from: https://www.w3schools.com/howto/howto_css_modals.asp
 
 // Get modal from DOM
-let modal = document.getElementById('instructions-modal');
+let instructionsModal = document.getElementById('instructions-modal');
 
 // Get button that opens instructions modal
 let instructionsModalButton = document.getElementById('instructions-btn');
@@ -11,10 +11,11 @@ let backButton = document.getElementsByClassName('back-button')[0];
 
 // Opens instructions modal when button is clicked
 instructionsModalButton.onclick = function () {
-    modal.style.display = "block";
+    instructionsModal.style.display = "block";
 }
 
 // Closes instructions modal when back button is clicked
 backButton.onclick = function () {
     modal.style.display = "none";
 }
+
