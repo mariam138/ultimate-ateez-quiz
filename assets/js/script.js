29a1aafd64d415e0aa49f30154892b709e69bc79
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-02-01 18:10:07 +0000

    feat: Add js to open/close settings modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Code to create modal is adapted from: https://www.w3schools.com/howto/howto_css_modals.asp
 
-// Get modal from DOM
+// Get instructions modal from DOM
 let instructionsModal = document.getElementById('instructions-modal');
 
 // Get button that opens instructions modal
 let instructionsModalButton = document.getElementById('instructions-btn');
 
-// Get button that will close modal
-let backButton = document.getElementsByClassName('back-button')[0];
+// Get button that will close instructions modal
+let backButtonOne = document.getElementsByClassName('back-button')[0];
 
 // Opens instructions modal when button is clicked
 instructionsModalButton.onclick = function () {
@@ -15,7 +15,25 @@ instructionsModalButton.onclick = function () {
 }
 
 // Closes instructions modal when back button is clicked
-backButton.onclick = function () {
-    modal.style.display = "none";
+backButtonOne.onclick = function () {
+    instructionsModal.style.display = "none";
 }
 
+// Get settings modal from DOM
+let settingsModal = document.getElementById('settings-modal');
+
+// Get button that opens settings modal
+let settingsModalButton = document.getElementById('settings-btn');
+
+// Get button that will close settings modal
+let backButtonTwo = document.getElementsByClassName('back-button')[1];
+
+// Opens settings modal
+settingsModalButton.onclick = function () {
+    settingsModal.style.display = "block";
+}
+
+// Closes settings modal
+backButtonTwo.onclick = function () {
+    settingsModal.style.display = "none";
+}
